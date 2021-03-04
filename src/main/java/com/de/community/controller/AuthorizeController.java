af8ff12727aba8fb49e1e20a34d643afb50a2183
imbazzc <931637821@qq.com>
2021-03-04 22:05:08 +0800

    add session

--- src/main/java/com/de/community/controller/AuthorizeController.java
+++ src/main/java/com/de/community/controller/AuthorizeController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -22,7 +25,9 @@ public class AuthorizeController {
     private String redirectUri;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code, @RequestParam(name="state") String state)
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request)
     {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -32,7 +37,12 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user !=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }
+        else {
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/com/de/community/dto/GithubUser.java
+++ src/main/java/com/de/community/dto/GithubUser.java
@@ -28,7 +28,12 @@ public class GithubUser {
         this.bio = bio;
     }
 
-
-
-
+    @Override
+    public String toString() {
+        return "GithubUser{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", bio='" + bio + '\'' +
+                '}';
+    }
 }
