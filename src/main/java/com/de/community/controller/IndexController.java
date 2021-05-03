78a4fcd382073e75eedc6c14295ce15c917d340e
imbazzc <931637821@qq.com>
2021-05-03 10:10:35 +0800

    aaa

--- src/main/java/com/de/community/controller/IndexController.java
+++ src/main/java/com/de/community/controller/IndexController.java
@@ -1,35 +1,25 @@
 package com.de.community.controller;
 
-import com.de.community.mapper.UserMapper;
-import com.de.community.model.User;
+import com.de.community.dto.PaginationDTO;
+import com.de.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+
     @Autowired
-    private UserMapper userMapper;
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null) {
-            return null;
+    public String index(Model model,
+                        @RequestParam(name="page",defaultValue = "1") Integer page,
+                        @RequestParam(name="size",defaultValue = "5") Integer size) {
+            PaginationDTO pagination = questionService.list(page,size);
+            model.addAttribute("pagination",pagination);
+            return "index";
         }
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if (user != null) {
-                    request.getSession().setAttribute("user", user);
-                }
-                break;
-                }
-            }
-        return "index";
     }
-}
--- src/main/java/com/de/community/dto/GithubUser.java
+++ src/main/java/com/de/community/dto/GithubUser.java
@@ -1,39 +1,12 @@
 package com.de.community.dto;
 
+import lombok.Data;
+
+@Data
 public class GithubUser {
     private String name;
     private long id;
     private String bio;
-
-    public String getName() {
-        return name;
+    private String avatarUrl;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
-}
